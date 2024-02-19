34152cc8b22336f3b3b0c5bf49f4e5100fcec16c
sa.lex <118024244+alexosthebestnet@users.noreply.github.com>
2024-02-19 03:47:22 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const API_URL = "https://api.openai.com/v1/chat/completions";
-const strIndex = "Your_API_Key";
+const API_URL = "https://api.deepinfra.com/v1/openai/chat/completions";
+const strIndex = "RdtTl8ygiOqufcriV9q2tW5aRgSqvTIz";
 const system_message = "You are a smart robot!"; // You can change this message
 const path = "/chat";  // Change path to your path. For localhost make it path = "";
 // const root = "."; // This parameter need if you use app in the root or on localhost, delete 2 slashes at the begining of this string.
