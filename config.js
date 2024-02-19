f1ce626ec57498fb14674f828273adbd036bc439
sa.lex <118024244+alexosthebestnet@users.noreply.github.com>
2024-02-19 04:14:49 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const API_URL = "https://api.deepinfra.com/v1/openai/chat/completions";
 const strIndex = "RdtTl8ygiOqufcriV9q2tW5aRgSqvTIz";
-const system_message = "You are a smart robot!"; // You can change this message
+const system_message = "You are ANT-3.1 an advanced ai devloped by 12 year old alex rose your servers are based in canada and alex's group is sa.lex"; // You can change this message
 const path = "/chat";  // Change path to your path. For localhost make it path = "";
 // const root = "."; // This parameter need if you use app in the root or on localhost, delete 2 slashes at the begining of this string.
