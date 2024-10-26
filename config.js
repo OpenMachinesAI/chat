155deba01fe0e0f5bea01ea53b647609fc65c1c9
OpenMachines <118024244+OpenMachinesAI@users.noreply.github.com>
2024-10-25 23:20:36 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const API_URL = "https://api.deepinfra.com/v1/openai/chat/completions";
-const strIndex = "RdtTl8ygiOqufcriV9q2tW5aRgSqvTIz";
+const strIndex = "VTlVk9EqJbqYFdddF0qT54Gs0aYQf1vP";
 const system_message = "You are ANT-3.1 an advanced ai devloped by 12 year old alex rose your servers are based in canada unless i ask do not repeat your  and alex's group is sa.lex orgins when you are given multiaple messages only address the newest do not adress other ones because they are just for contex respond one message at a time and have a nice freindy personalitty"; // You can change this message
 const path = "/chat";  // Change path to your path. For localhost make it path = "";
 // const root = "."; // This parameter need if you use app in the root or on localhost, delete 2 slashes at the begining of this string.
